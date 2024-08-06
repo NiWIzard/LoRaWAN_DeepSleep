99958ab867f66ea250ca1a6bfe5622e399fc5945
NiWIzard <nisarat.k67@gmail.com>
2024-08-06 11:44:12 +0700

    LoRaWAN_DeepSleepESP32
    it can save energy and transmit more than 11 bytes of data.

---
+++ LoRaWAN_DeepSleepMode.ino
@@ -0,0 +1,459 @@
+// Board : ESP32 Wrovser Module
+// Nisarat K
+// This can sleep mode and send more 11 byte. 
+// 1.8.2024 
+
+#include <lmic.h>
+#include <hal/hal.h>
+#include <SPI.h>
+//ET-BME280
+#include <Wire.h>
+
+//deep sleep mode
+// Saves the LMIC structure during DeepSleep
+RTC_DATA_ATTR lmic_t RTC_LMIC;
+bool GOTO_DEEPSLEEP = false;
+int cout_timer;
+int count_numb = 0;
+
+
+// LoRaWan
+// ต้องสลับเลขจากหลังไปหน้า 
+static const u1_t PROGMEM APPEUI[8]={ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+void os_getArtEui (u1_t* buf) { memcpy_P(buf, APPEUI, 8);}
+
+//ต้องสลับเลขจากหลังไปหน้า
+static const u1_t PROGMEM DEVEUI[8]={ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+void os_getDevEui (u1_t* buf) { memcpy_P(buf, DEVEUI, 8);}
+                                         
+static const u1_t PROGMEM APPKEY[16] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+void os_getDevKey (u1_t* buf) {  memcpy_P(buf, APPKEY, 16);}
+uint8_t mydata[]="Hello";
+static osjob_t sendjob;
+
+// Time deep sleep mode.
+const unsigned TX_INTERVAL = 300;
+// Schedule TX every this many seconds (might become longer due to duty
+// cycle limitations).
+const unsigned TX_INTERVAL_LoRa = 60;
+int sequnNow;
+
+// Pin mapping
+const lmic_pinmap lmic_pins = {
+    .nss = 5,
+    .rxtx = LMIC_UNUSED_PIN,
+    .rst = 13,
+    .dio = {2, 15, LMIC_UNUSED_PIN},
+};
+
+void printHex2(unsigned v) {
+    v &= 0xff;
+    if (v < 16)
+        Serial.print('0');
+        Serial.print(v, HEX);
+}
+
+void onEvent (ev_t ev) {
+    Serial.print(os_getTime());
+    Serial.print(": ");
+    switch(ev) {
+        case EV_SCAN_TIMEOUT:
+            Serial.println(F("EV_SCAN_TIMEOUT"));
+            break;
+        case EV_BEACON_FOUND:
+            Serial.println(F("EV_BEACON_FOUND"));
+            break;
+        case EV_BEACON_MISSED:
+            Serial.println(F("EV_BEACON_MISSED"));
+            break;
+        case EV_BEACON_TRACKED:
+            Serial.println(F("EV_BEACON_TRACKED"));
+            break;
+        case EV_JOINING:
+            Serial.println(F("EV_JOINING"));
+            break;
+        case EV_JOINED:
+            Serial.println(F("EV_JOINED"));
+            {
+              u4_t netid = 0;
+              devaddr_t devaddr = 0;
+              u1_t nwkKey[16];
+              u1_t artKey[16];
+              LMIC_getSessionKeys(&netid, &devaddr, nwkKey, artKey);
+              Serial.print("netid: ");
+              Serial.println(netid, DEC);
+              Serial.print("devaddr: ");
+              Serial.println(devaddr, HEX);
+              Serial.print("AppSKey: ");
+              for (size_t i=0; i<sizeof(artKey); ++i) {
+                if (i != 0)
+                  Serial.print("-");
+                printHex2(artKey[i]);
+              }
+              Serial.println("");
+              Serial.print("NwkSKey: ");
+              for (size_t i=0; i<sizeof(nwkKey); ++i) {
+                      if (i != 0)
+                              Serial.print("-");
+                      printHex2(nwkKey[i]);
+              }
+              Serial.println();
+            }
+            // Disable link check validation (automatically enabled
+            // during join, but because slow data rates change max TX
+            // size, we don't use it in this example.
+
+            
+            LMIC_setLinkCheckMode(0);
+            break;
+        /*
+        || This event is defined but not used in the code. No
+        || point in wasting codespace on it.
+        ||
+        || case EV_RFU1:
+        ||     Serial.println(F("EV_RFU1"));
+        ||     break;
+        */
+        case EV_JOIN_FAILED:
+            Serial.println(F("EV_JOIN_FAILED"));
+            break;
+        case EV_REJOIN_FAILED:
+            Serial.println(F("EV_REJOIN_FAILED"));
+            break;
+        case EV_TXCOMPLETE:
+            Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
+            if (LMIC.txrxFlags & TXRX_ACK)
+              Serial.println(F("Received ack"));
+            if (LMIC.dataLen) {
+              Serial.print(F("Received "));
+              Serial.print(LMIC.dataLen);
+              Serial.println(F(" bytes of payload"));
+            }
+            //                    :)
+            if(sequnNow == 0){
+              os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL_LoRa), do_send);
+            }else if(sequnNow > 0){
+              GOTO_DEEPSLEEP = true;             
+            }else{
+              Serial.println("LoRa ERROR");
+            }
+            break;
+        case EV_LOST_TSYNC:
+            Serial.println(F("EV_LOST_TSYNC"));
+            break;
+        case EV_RESET:
+            Serial.println(F("EV_RESET"));
+            break;
+        case EV_RXCOMPLETE:
+            // data received in ping slot
+            Serial.println(F("EV_RXCOMPLETE"));
+            break;
+        case EV_LINK_DEAD:
+            Serial.println(F("EV_LINK_DEAD"));
+            break;
+        case EV_LINK_ALIVE:
+            Serial.println(F("EV_LINK_ALIVE"));
+            break;
+        /*
+        || This event is defined but not used in the code. No
+        || point in wasting codespace on it.
+        ||
+        || case EV_SCAN_FOUND:
+        ||    Serial.println(F("EV_SCAN_FOUND"));
+        ||    break;
+        */
+        case EV_TXSTART:
+            Serial.println(F("EV_TXSTART"));
+            break;
+        case EV_TXCANCELED:
+            Serial.println(F("EV_TXCANCELED"));
+            break;
+        case EV_RXSTART:
+            /* do not print anything -- it wrecks timing */
+            break;
+        case EV_JOIN_TXCOMPLETE:
+            Serial.println(F("EV_JOIN_TXCOMPLETE: no JoinAccept"));
+            break;
+
+        default:
+            Serial.print(F("Unknown event: "));
+            Serial.println((unsigned) ev);
+            break;
+    }
+}
+
+//int j=0;
+void do_send(osjob_t* j){
+    // Check if there is not a current TX/RX job running
+   
+    if (LMIC.opmode & OP_TXRXPEND) {  
+        Serial.println(F("OP_TXRXPEND, not sending"));
+    } else {         
+      // Prepare upstream data transmission at the next possible time.
+       int XX = LMIC_setTxData2(1, mydata, sizeof(mydata)-1, 0);
+       Serial.println(F(mydata));
+       Serial.println(sizeof(mydata));
+       Serial.println(F("Packet queued"));
+    }
+    // Next TX is scheduled after TX_COMPLETE event.
+}
+
+void PrintRuntime()
+{
+    long seconds = millis() / 1000;
+    Serial.print("Runtime: ");
+    Serial.print(seconds);
+    Serial.println(" seconds");
+}
+
+void deepSleepMode(){
+    
+    Serial.println("Node sensor going to deep");
+    PrintRuntime();
+    Serial.flush();
+    esp_sleep_enable_timer_wakeup(TX_INTERVAL * 1000000);
+    esp_deep_sleep_start();
+}
+
+void SaveLMICToRTC(int deepsleep_sec)
+{
+    Serial.println(F("Save LMIC to RTC"));
+    RTC_LMIC = LMIC;
+
+    // ESP32 can't track millis during DeepSleep and no option to advanced millis after DeepSleep.
+    // Therefore reset DutyCyles
+
+    unsigned long now = millis();
+
+    // EU Like Bands
+#if defined(CFG_LMIC_EU_like)
+    Serial.println(F("Reset CFG_LMIC_EU_like band avail"));
+    for (int i = 0; i < MAX_BANDS; i++)
+    {
+        ostime_t correctedAvail = RTC_LMIC.bands[i].avail - ((now / 1000.0 + deepsleep_sec) * OSTICKS_PER_SEC);
+        if (correctedAvail < 0)
+        {
+            correctedAvail = 0;
+        }
+        RTC_LMIC.bands[i].avail = correctedAvail;
+    }
+
+    RTC_LMIC.globalDutyAvail = RTC_LMIC.globalDutyAvail - ((now / 1000.0 + deepsleep_sec) * OSTICKS_PER_SEC);
+    if (RTC_LMIC.globalDutyAvail < 0)
+    {
+        RTC_LMIC.globalDutyAvail = 0;
+    }
+#else
+    Serial.println(F("No DutyCycle recalculation function!"));
+#endif
+}
+
+void LoadLMICFromRTC()
+{
+    Serial.println(F("Load LMIC from RTC"));
+    LMIC = RTC_LMIC;
+}
+
+void LoraWANDebug(lmic_t lmic_check)
+{
+    Serial.println("");
+    Serial.println("");
+
+    //LoraWANPrintLMICOpmode();
+    Serial.println("");
+
+    Serial.print(F("LMIC.seqnoUp = "));
+    Serial.println(lmic_check.seqnoUp);
+
+    //ni debug
+    Serial.print(F("LMIC.dataLen = "));
+    Serial.println(lmic_check.dataLen);
+    Serial.print(F("LMIC.txCnt = "));
+    Serial.println(lmic_check.txCnt);
+    Serial.print(F("LMIC.txrxflags = "));
+    Serial.println(lmic_check.txrxFlags);
+
+    Serial.print(F("LMIC.globalDutyRate = "));
+    Serial.print(lmic_check.globalDutyRate);
+    Serial.print(F(" osTicks, "));
+    Serial.print(osticks2ms(lmic_check.globalDutyRate) / 1000);
+    Serial.println(F(" sec"));
+
+    Serial.print(F("LMIC.globalDutyAvail = "));
+    Serial.print(lmic_check.globalDutyAvail);
+    Serial.print(F(" osTicks, "));
+    Serial.print(osticks2ms(lmic_check.globalDutyAvail) / 1000);
+    Serial.println(F(" sec"));
+
+    Serial.print(F("LMICbandplan_nextTx = "));
+    Serial.print(LMICbandplan_nextTx(os_getTime()));
+    Serial.print(F(" osTicks, "));
+    Serial.print(osticks2ms(LMICbandplan_nextTx(os_getTime())) / 1000);
+    Serial.println(F(" sec"));
+
+    Serial.print(F("os_getTime = "));
+    Serial.print(os_getTime());
+    Serial.print(F(" osTicks, "));
+    Serial.print(osticks2ms(os_getTime()) / 1000);
+    Serial.println(F(" sec"));
+
+    Serial.print(F("LMIC.txend = "));
+    Serial.println(lmic_check.txend);
+    Serial.print(F("LMIC.txChnl = "));
+    Serial.println(lmic_check.txChnl);
+
+    Serial.println(F("Band \tavail \t\tavail_sec\tlastchnl \ttxcap"));
+    for (u1_t bi = 0; bi < MAX_BANDS; bi++)
+    {
+        Serial.print(bi);
+        Serial.print("\t");
+        Serial.print(lmic_check.bands[bi].avail);
+        Serial.print("\t\t");
+        Serial.print(osticks2ms(lmic_check.bands[bi].avail) / 1000);
+        Serial.print("\t\t");
+        Serial.print(lmic_check.bands[bi].lastchnl);
+        Serial.print("\t\t");
+        Serial.println(lmic_check.bands[bi].txcap);
+    }
+    Serial.println("");
+    Serial.println("");
+  
+}
+
+void getSeqn(lmic_t lmic_check){
+    Serial.println("");
+
+    // Serial.print(F("LMIC.seqnoUp = "));
+    // Serial.println(lmic_check.seqnoUp);
+    sequnNow = lmic_check.seqnoUp;
+    // Serial.print(F("LMIC.datarate = "));
+    // Serial.println(lmic_check.datarate);
+
+}
+void LoraWANPrintLMICOpmode(void)
+{
+    Serial.print(F("LMIC.opmode: "));
+    if (LMIC.opmode & OP_NONE)
+    {
+        Serial.print(F("OP_NONE "));
+    }
+    if (LMIC.opmode & OP_SCAN)
+    {
+        Serial.print(F("OP_SCAN "));
+    }
+    if (LMIC.opmode & OP_TRACK)
+    {
+        Serial.print(F("OP_TRACK "));
+    }
+    if (LMIC.opmode & OP_JOINING)
+    {
+        Serial.print(F("OP_JOINING "));
+    }
+    if (LMIC.opmode & OP_TXDATA)
+    {
+        Serial.print(F("OP_TXDATA "));
+    }
+    if (LMIC.opmode & OP_POLL)
+    {
+        Serial.print(F("OP_POLL "));
+    }
+    if (LMIC.opmode & OP_REJOIN)
+    {
+        Serial.print(F("OP_REJOIN "));
+    }
+    if (LMIC.opmode & OP_SHUTDOWN)
+    {
+        Serial.print(F("OP_SHUTDOWN "));
+    }
+    if (LMIC.opmode & OP_TXRXPEND)
+    {
+        Serial.print(F("OP_TXRXPEND "));
+    }
+    if (LMIC.opmode & OP_RNDTX)
+    {
+        Serial.print(F("OP_RNDTX "));
+    }
+    if (LMIC.opmode & OP_PINGINI)
+    {
+        Serial.print(F("OP_PINGINI "));
+    }
+    if (LMIC.opmode & OP_PINGABLE)
+    {
+        Serial.print(F("OP_PINGABLE "));
+    }
+    if (LMIC.opmode & OP_NEXTCHNL)
+    {
+        Serial.print(F("OP_NEXTCHNL "));
+    }
+    if (LMIC.opmode & OP_LINKDEAD)
+    {
+        Serial.print(F("OP_LINKDEAD "));
+    }
+    if (LMIC.opmode & OP_LINKDEAD)
+    {
+        Serial.print(F("OP_LINKDEAD "));
+    }
+    if (LMIC.opmode & OP_TESTMODE)
+    {
+        Serial.print(F("OP_TESTMODE "));
+    }
+    if (LMIC.opmode & OP_UNJOIN)
+    {
+        Serial.print(F("OP_UNJOIN "));
+    }
+}
+
+void setup() {
+    cout_timer = 0;
+    Serial.begin(9600);
+  
+    while (!Serial) {
+      // Serial.println("loop for init serial 0"); 
+    }
+    Serial.println("start init software serial");
+             
+    // LMIC init
+    os_init();
+    // Reset the MAC state. Session and pending data transfers will be discarded.
+    LMIC_reset();
+
+    if (RTC_LMIC.seqnoUp != 0)
+    {
+        LoadLMICFromRTC();           
+    }
+    LoraWANDebug(LMIC);
+    // Start job (sending automatically starts OTAA too)
+    do_send(&sendjob);
+   // delay(1000);
+
+}
+
+
+
+void loop()
+{
+    static unsigned long lastPrintTime = 0;
+    os_runloop_once();
+
+    const bool timeCriticalJobs = os_queryTimeCriticalJobs(ms2osticksRound((TX_INTERVAL_LoRa * 1000)));
+    if (!timeCriticalJobs && GOTO_DEEPSLEEP == true && !(LMIC.opmode & OP_TXRXPEND))
+    {   
+        Serial.print(F("Can go sleep "));
+        LoraWANPrintLMICOpmode();
+        SaveLMICToRTC(TX_INTERVAL);
+        deepSleepMode();
+    }
+    else if (lastPrintTime + 2000 < millis())
+    {
+        Serial.print(F("Cannot sleep "));
+        getSeqn(LMIC);
+        LoraWANPrintLMICOpmode();
+        PrintRuntime();
+       // LoraWANDebug(LMIC);
+       
+        lastPrintTime = millis();
+    }
+}
+
+
+
